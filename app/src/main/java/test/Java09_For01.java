79525e1173b02154b27dc503c00ca5991c20ba54
BAOzhu8687 <zhouzongxin_94@163.com>
2019-11-19 17:53:46 +0800

    switch2019.11.19

--- app/src/main/java/test/Java09_For01.java
+++ app/src/main/java/test/Java09_For01.java
@@ -1,6 +1,6 @@
 package test;
 /*
-* 在前一个程序的基础之上，计算1-100所有奇数的和*/
+* 在前一个程序的基础之上，计算1-100所有奇数的和。*/
 public class Java09_For01 {
     public static void main(String[] agse){
         for (int i=1;i<=100;i+=2){
