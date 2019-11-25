e0f45b3bdff94aeae3bb4e73dae895501942b23e
BAOzhu8687 <zhouzongxin_94@163.com>
2019-11-25 17:00:17 +0800

    switch2019.11.25

--- app/src/main/java/test/Java09_For01.java
+++ app/src/main/java/test/Java09_For01.java
@@ -1,10 +1,20 @@
 package test;
 /*
-* 在前一个程序的基础之上，计算1-100所有奇数的和。*/
+* */
 public class Java09_For01 {
     public static void main(String[] agse){
-        for (int i=1;i<=100;i+=2){
-
+        for(int i=1;i<=10;i+=2){
+            System.out.println(i);//奇数
+        }
+        for(int i=0;i<10;i+=2){
+            System.out.println(i);//偶数
+        }
+        for(int i=0;i<10;i-=2){
+            System.out.println(i);
+        }
+        for(int i=0;i<10;) {
+            System.out.println(i);
+            i++;
         }
     }
 }
