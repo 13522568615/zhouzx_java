840bb4faa60471a923627af1c2410b8c77d092bc
BAOzhu8687 <zhouzongxin_94@163.com>
2019-11-25 17:24:52 +0800

    switch2019.11.25

--- app/src/main/java/test/Java10_while.java
+++ app/src/main/java/test/Java10_while.java
@@ -1,4 +1,18 @@
 package test;
-
+/*
+*       while循环的语法结构
+*           while(布尔表达式){
+*               循环体：
+*           }
+*       执行为true执行，false结束
+*
+* */
 public class Java10_while {
+    public static void main(String[] agse){
+/*        while(true){//死循环
+            System.out.println("");*/
+
+
+        //}
+    }
 }
