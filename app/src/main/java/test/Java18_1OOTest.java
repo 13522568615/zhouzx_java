51537c5b76760ef663d182e1a92b620d00640a98
BAOzhu8687 <zhouzongxin_94@163.com>
2019-12-26 17:12:54 +0800

    switch2019.12.26

--- app/src/main/java/test/Java18_1OOTest.java
+++ app/src/main/java/test/Java18_1OOTest.java
@@ -4,6 +4,8 @@ package test;
     112集
 
 */
+
+
 public class Java18_1OOTest {
 
 
@@ -96,3 +98,27 @@ public class Java18_1OOTest {
 
     实例变量是一个对象一份，100个对象有100份
 * */
+
+
+class Java18_Student {
+    //类体 = 属性 + 方法
+
+    //属性【存储数据采用变量的形式】
+    //由于变量定义在类体当中，方法体之外，这种变量称为成员变量
+
+    //对象被称为实例，实例变量又被称为对象变量
+    //不创建对象，这个no变量的内存空间是不存在的
+    int no;
+
+    //姓名
+    String name;
+
+    //年龄
+    int age;
+
+    //性别
+    boolean sex;
+
+    //住址
+    String addr;
+}
