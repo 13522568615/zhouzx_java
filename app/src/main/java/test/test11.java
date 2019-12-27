ec0641601beff59a9cd9b2a564afaa6b0a991e48
BAOzhu8687 <zhouzongxin_94@163.com>
2019-12-27 19:10:15 +0800

    2019.12.27封装

---
+++ app/src/main/java/test/test11.java
@@ -0,0 +1,24 @@
+package test;
+
+public class test11 {
+    private String age;
+    private int id;
+
+
+    public void setAge(String a) {
+        age = a;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String setAge() {
+        return age;
+    }
+}
